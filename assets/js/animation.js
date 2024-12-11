65803102a0ea7e92c47bce385893fa35b24124d8
Akihi <2579500994@qq.com>
2024-12-11 15:32:13 +0800

    update

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -72,3 +72,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+document.querySelectorAll('a[href^="#fn:"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            const headerHeight = document.querySelector('.header').offsetHeight;
+            window.scrollTo({
+                top: targetElement.offsetTop - headerHeight,
+                behavior: 'smooth' // 平滑滚动
+            });
+        }
+    });
+});
